3731ad3cd46a5bd42baa4d13e64fc2b07e2fdec4
linyaqi <mr03web@foxmail.com>
2019-01-31 11:41:16 +0800

    service

--- cotra-cloud-starter/service-hello/src/main/java/github/cotra/IndexController.java
+++ cotra-cloud-starter/service-hello/src/main/java/github/cotra/IndexController.java
@@ -1,22 +1,17 @@
 package github.cotra;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class IndexController {
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("/hello")
+    public String home() {
+        return "hi " + "i am from port:" + port;
     }
 }
